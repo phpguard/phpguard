6b93d9c2de69760104274584320540d5cdc9912b
Anthonius Munthi <me@itstoni.com>
2014-05-20 17:54:39 +0800

    added base ObjectBehavior for phpguard

---
+++ src/PhpGuard/Application/Spec/ObjectBehavior.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the PhpGuard project.
+ *
+ * (c) Anthonius Munthi <me@itstoni.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PhpGuard\Application\Spec;
+
+use PhpSpec\ObjectBehavior as BaseObjectBehavior;
+
+/**
+ * Class ObjectBehavior
+ *
+ * @package PhpGuard\Application\Spec
+ * @codeCoverageIgnore
+ */
+abstract class ObjectBehavior extends BaseObjectBehavior
+{
+    static public $tmpDir;
+    static public function mkdir($dir)
+    {
+        @mkdir($dir,0755,true);
+    }
+
+    static public function cleanDir($dir)
+    {
+        if (!is_dir($dir)) {
+            return;
+        }
+
+        $flags = \FilesystemIterator::SKIP_DOTS;
+        $iterator = new \RecursiveDirectoryIterator($dir, $flags);
+        $iterator = new \RecursiveIteratorIterator(
+            $iterator, \RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($iterator as $path) {
+            if ($path->isDir()) {
+                @rmdir((string) $path);
+            } else {
+                @unlink((string) $path);
+            }
+        }
+
+        @rmdir($dir);
+    }
+}
+
+ObjectBehavior::$tmpDir = sys_get_temp_dir().'/phpguard-test';
