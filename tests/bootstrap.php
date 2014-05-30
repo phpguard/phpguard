861310c9511a9fc39c732d1e3a66e3fa38b08a33
Anthonius Munthi <me@itstoni.com>
2014-05-31 00:54:31 +0800

    prepared plugin separation

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,4 @@ $loader = require __DIR__ . '/../vendor/autoload.php';
 
 $basedir = realpath(__DIR__.'/..');
 $loader->addPsr4('PhpGuard\\Application\\Functional\\', __DIR__.'/functional');
-$loader->addPsr4('PhpGuard\\Plugins\\PhpSpec\\Functional\\', $basedir.'/plugins/phpspec/functional');
-$loader->addPsr4('PhpGuard\\Plugins\\PHPUnit\\Functional\\', $basedir.'/plugins/phpunit/functional');
 $loader->register();
